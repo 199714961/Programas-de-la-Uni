2f0ece5bffb88a6edde053a0054606e015262470
199714961 <49260606+199714961@users.noreply.github.com>
2020-05-08 16:44:27 -0500

    Add files via upload
    Esta es la primera tarea de la asignatura "Lenguajes de programación"

---
+++ Programa en perl 1.pl
@@ -0,0 +1,124 @@
+#!C:\Strawberry\perl\bin\perl.exe
+
+#1.-Aparece la cara de saw dibujada con puros 6's
+#   La cara de saw aparecerá poco a poco gracias a un for con i=numero de filas de 6's en saw
+
+use strict;
+
+printf("Quiero jugar un juego, estas aburrido?\n");
+printf("1.- SI\n");
+printf("2.- NO\n");
+my $r = <STDIN>;
+printf("\n");
+printf("Da igual en realidad...\n");
+printf("Haz sido sentenciado por tus pecados, asi que deberas jugar mi juego de todas formas c:\n");
+printf("\n");
+
+printf("1.- Quien me sentencio?\n");
+printf("2.- Que pecados cometi?\n");
+printf("3.- mE perdONAs?\n");
+printf("\n");
+my $r1 = <STDIN>;
+
+if (($r1 <=> 1) eq 0 ) {
+   printf("YO CLARAMENTE JAJAJ\n");
+}
+elsif (($r1 <=> 2) eq 0 ) {
+   printf("Hackeaste el Pc de tu profesor de calculo para salvar el ramo\n");
+   printf("SI QUIERES SER INGENIERO, ESTUDIA!\n");
+}
+else {
+   printf("NO\n");
+}
+printf("\n");
+
+printf("Ahora bien, cual era tu nombre?");
+my $nombre=<STDIN>;
+printf("\n");
+printf($nombre.", sabes quien soy yo verdad?\n");
+printf("\n");
+printf("1.- SI\n");
+printf("2.- NO\n");
+printf("3.- Hoch?");
+my $r2 = <STDIN>;
+printf("\n");
+if (($r2 <=> 1) eq 0 ) {
+   printf("Que astuto...\n");
+}
+elsif (($r2 <=> 2) eq 0 ) {
+   printf("y ahora?...\n");
+}
+else {
+   print("jaja, no\n");
+}
+printf("\n");
+system ('C:\Users\alexs\Desktop\musica\saw.mp3');
+
+printf("ME RECONOCES AHORA?\n");
+printf("\n");
+printf("1.- Eres Saw...\n");
+printf("2.- Nop\n");
+printf("3.- Eres Hosh?");
+printf("\n");
+my $r3=<STDIN>;
+printf("\n");
+if (($r3 <=> 1) eq 0 ) {
+   printf("jaja claro que si, y si no quieres perder tu informacion deberas responder correctamente la siguiente pregunta:\n");
+}
+elsif (($r3 <=> 2) eq 0 ) {
+   printf("en fin... deberas responder correctamente la siguiente pregunta, a menos que quieras perder toda la informacion que tiene tu pc...\n");
+}
+else {
+   printf("...");
+   printf("en fin... deberas responder correctamente las siguientes preguntas si no quieres perder tu informacion");
+}
+
+printf("\n");
+printf("1.-Lanzala Crack, Im ready!\n");
+printf("2.-Cuanto tiempo tengo?\n");
+printf("\n");
+my $r4 = <STDIN>;
+printf("\n");
+if (($r4 <=> 1) eq 0 ) {
+	printf("...");
+   printf("Tienes un minuto para responder, tranquilo es facil c:");
+}
+else {
+	printf("Solo un minuto... tranquilo es sencillo c:");
+}
+
+printf("\n");
+printf("1.-Esta bien...\n");
+printf("2.-Podrian ser 2?\n");
+
+my $r5 = <STDIN>;
+printf("\n");
+printf("Preparado...");
+my $pausa1 = <STDIN>;
+printf("LISTO...");
+my $pausa2 = <STDIN>;
+printf("\n");
+system('shutdown -s -t 60');
+printf("PROBLEMA");
+printf("\n");
+printf("Si un profesor DE 70 anios se pasa 5 horas de su vida haciendo una prueba y recibe un supuesto correo del\n departamento de matematicas pidiendole que descargue un documento .exe... ");
+my $pausa2 = <STDIN>;
+printf("CALCULE LA GRAVEDAD DE JUPITER MEDIDA EN GIGAWATTS REPTILIANOS! ");
+printf("\n");
+printf("1.- Alguna vez te dije lo genial que son tus peliculas?\n");
+printf("2.- Por favor, tengo recuerdos de mi perro en mi computador :c\n");
+printf("3.- 1000?\n");
+printf("\n");
+my $r9=<STDIN>;
+
+my $v1 = 1;
+my $v2 = 1;
+
+while (($v1 <=> $v2) eq 0 ) {
+   printf("incorrecto... DESPEDITE DE TUS DATOS CRACK JAJAJAJAJAJAJ\n");
+}
+
+
+
+
+
