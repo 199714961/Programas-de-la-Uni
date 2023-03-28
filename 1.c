58450932a2420919bf8cafcfce5a23864cf72d6e
virtualhomeless <virtualhomeless846665@gmail.com>
2023-03-28 07:01:22 -0600

    b

--- 1.c
+++ 1.c
@@ -7,13 +7,4 @@ void main(){
 	scanf("%d",&precio); //"scanf" para leer el dato de usuario, "&" para concatenar (?
 	printf("Precio:  %d", precio);
 	
-	
-	printf("\nInserta el porcentaje de descuento:");
-	scanf("%d",&descuento);
-    descuentoDecimal=(float)descuento/100;//Este decimal lo toma como un numero negativo...wtf?
-    descuento=precio*descuentoDecimal;
-    printf("\nDescuento decimal: %d", descuentoDecimal,"");
-    printf("\nDescuento real: %d",descuento,"");
-    printf("\n\nEl valor total del producto es: %d",precio-descuento);
-	
 }
